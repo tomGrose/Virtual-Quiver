c118f0293a2a2ea9f750510d1d14263e25e3484e
Thomas Rosenbaugh <tomgrosenbaugh@gmail.com>
2021-04-13 11:48:57 -0600

    Enabled flask caching and added a favicon

--- app.py
+++ app.py
@@ -558,19 +558,3 @@ def show_about():
 @app.route('/information')
 def show_info():
     return render_template('info.html')
-
-
-# ##############################################################################
-# # Turn off all caching in Flask
-# #
-# # https://stackoverflow.com/questions/34066804/disabling-caching-in-flask
-
-@app.after_request
-def add_header(req):
-    """Add non-caching headers on every request."""
-
-    req.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    req.headers["Pragma"] = "no-cache"
-    req.headers["Expires"] = "0"
-    req.headers['Cache-Control'] = 'public, max-age=0'
-    return req
