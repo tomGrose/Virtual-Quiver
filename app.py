80f8b37386d55b7b26148b50ff7e9a3506626f89
Thomas Rosenbaugh <tomgrosenbaugh@gmail.com>
2021-04-13 10:36:04 -0600

    Set up environment variables for heroku

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ app.config['REMEMBER_COOKIE_DURATION'] = timedelta(days=7)
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'virtualquiverdiscs@gmail.com'
-app.config['MAIL_PASSWORD'] = '80222Denver!'
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', 'virtualquiverdiscs@gmail.com')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
 toolbar = DebugToolbarExtension(app)
 mail = Mail(app)
